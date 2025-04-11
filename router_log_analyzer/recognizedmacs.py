f92c8af64db554a45ea59ad08e9a4b1c92cdf602
c <gkby4242@gmx.com>
2025-04-11 11:15:47 -0400

    Add the scrubbed recognized macs lookup table file

---
+++ router_log_analyzer/recognizedmacs.py
@@ -0,0 +1,14 @@
+"""
+Listing of recognized MAC addresses.
+    MAC addresses here are fake.
+"""
+
+
+recognizedmacaddresses = dict(
+    [
+        ("AA:66:FF:B6:99:72", "Other Device"),
+        ("1A:10:12:84:72:76", "DESKTOP 5"),
+        ("F0:00:10:50:09:50", "DESKTOP 10"),
+        ("55:CC:EA:DF:DD:AA", "My Laptop"),
+    ]
+)
